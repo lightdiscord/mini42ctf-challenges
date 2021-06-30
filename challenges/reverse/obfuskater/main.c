69b2473c6696d6e1a17684eec3d4ba3a4f32187a
arpascal <arpascal@student.42lyon.fr>
2021-06-30 14:44:53 +0200

    unreadable

--- challenges/reverse/obfuskater/main.c
+++ challenges/reverse/obfuskater/main.c
@@ -6,8 +6,9 @@
 
 #define YOU ("How are you doing?")
 #define WEIRD (":heavy_thinking:")
+#define AMAZING ("This sure is amazing")
 #define SICK ("I don't know what I should write here so here's a long string")
-
+#define NORM ("Norm go brrrrrrr")
 #define PARENTHESES_ARE_OVERKILL ("Congratzzz!")
 
 static const char	*g_thinking = (void*)42;
@@ -16,11 +17,9 @@ t_check	one(const char *str, size_t index, unsigned int value)
 {
 	if ((index | value) == 0)
 		return ((t_check)two);
-	if ((*((unsigned int *)str) ^ value) != ((unsigned int *)g_thinking)[index])
-	{
-		printf("%x\n", ((unsigned int *)g_thinking)[index]);
+	if ((value ^ *((unsigned int *)str) ^ value)
+		!= (((unsigned int *)g_thinking)[index] ^ value))
 		exit(1);
-	}
 	return ((t_check)two);
 }
 
@@ -35,9 +34,13 @@ int	main(int argc, const char *const *argv)
 {
 	if (argv[1] && strlen(argv[1]) == 40)
 	{
-		one (argv[1], 0, 0). hi YOU. fn (argv[1], 0, 1226376741). this_is WEIRD. fn(argv[1] + 4, 1, 538725186). \
-			this_is SICK. fn(argv[1] + 8, 9, 437607203) \
-			;
+		one (argv[1], 0, 0). hi YOU. fn (argv[1], 0, 1226376741). this_is \
+			WEIRD. fn(argv[1] + 4, 1, 538725186). this_is SICK. fn(argv[1] + 8,
+			9, 437607203). hi NORM. fn(argv[1] + 12, 4, 1561201518). this_is
+		AMAZING. fn(argv[1] + 16, 2, 121000966). whut NORM. fn(argv[1] + 20,
+			0, 838950438). whut WEIRD. fn(argv[1] + 24, 0, 1410751562). hi
+		SICK. fn(argv[1] + 28, 4, 320951339). fnptrgobrr YOU. fn(argv[1] + 32,
+			1, 1381309502). hi SICK. fn(argv[1] + 36, 3, 474041677);
 		puts \
 			PARENTHESES_ARE_OVERKILL;
 	}
--- challenges/reverse/obfuskater/main.h
+++ challenges/reverse/obfuskater/main.h
@@ -7,6 +7,8 @@ typedef union u_check {
 	union	u_check (*fn)(const char *str, size_t index, unsigned int value);
 	union	u_check (*hi)(const char *str);
 	union	u_check (*this_is)(const char *str);
+	union	u_check (*whut)(const char *str);
+	union	u_check (*fnptrgobrr)(const char *str);
 }	t_check;
 
 t_check	one(const char *str, size_t index, unsigned int value);
